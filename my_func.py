6b2f18e4b169930c60f639a8fd662074f50fb5fa
oliviaguest <guest.olivia@gmail.com>
2017-11-07 16:38:11 +0000

    new rubbish

---
+++ my_func.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+def my_func(a, b):
+    if a == 1 and b == 1:
+        return 0
+    return a + b
+
+if __name__ == '__main__':
+    print(my_func(200, 500))
--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from __future__ import print_function
-import numpy as np
+import unittest
 
-x = 2
-print(x**2)
-print("55555")
-print('hello')
+from my_func import my_func
+
+
+class TestMy_Sum(unittest.TestCase):
+    """Test pdist."""
+
+    def test_my_func(self):
+        self.assertTrue(my_func(3, 5) == 8)
+        self.assertTrue(my_func(1000, 2000) == 3000)
+        self.assertTrue(my_func(1, 1) == 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
